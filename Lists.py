99f21e75c3b1d9d6bbe261d75bfb84831b2e72c5
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 15:33:04 -0400

    Using Lists in python

---
+++ Lists.py
@@ -0,0 +1,17 @@
+# the code below prints values of i with an increment of 1 and compares it till it meets the condition
+# ----------------------------------
+names = ["John", "Bob", "Debola", "mosh"]
+print(names)  # prints it exactly as declared
+
+# print the string at an index
+# ----------------------------------
+print(names[0])  # prints the first string starting at index 0, next string is index 1 .....
+
+# replace the string at an index
+# ----------------------------------
+names[2] = "Modesire"
+print(names)
+
+# Print a list of names
+# ----------------------------------
+print(names[0:3])  # it will print names  at index 0, 1, 2
