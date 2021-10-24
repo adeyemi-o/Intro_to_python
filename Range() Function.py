d275f463c7b57c790f40c8836baa0eea4d11e4c3
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 17:05:28 -0400

    Range() function

---
+++ Range() Function.py
@@ -0,0 +1,10 @@
+# the code below will print a list of numbers btw 5 and 10 with an increment of 2
+# -------------------------------------------------------------------------------
+numbers = range(5, 10, 2)
+for i in numbers:
+    print(i)
+
+# code above simplified
+# ---------------------
+for i in range(5, 10, 2):
+    print(i)
