052b74f514d5b4f34e380f92fe4a23c9a31de7d3
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 16:58:02 -0400

    For Loops

---
+++ For Loops.py
@@ -0,0 +1,10 @@
+# remove & clear method
+# ----------------------------------
+numbers = [1, 2, 3, 4, 5]
+for item in numbers:  # this is a for Loop
+    print(item)
+
+i = 0
+while i < len(numbers):
+    print(numbers[i])
+    i = i + 1
