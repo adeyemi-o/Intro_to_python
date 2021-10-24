b5cf405eac40fd7b077e91641afb788d0c93cb8e
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 11:22:12 -0400

    using a function/method with strings Arithmetic operators Comparison Operators

--- intro-continued.py
+++ intro-continued.py
@@ -5,7 +5,10 @@ print(course.find('P'))  # finds the index of a character
 print(course.replace('for', '4'))  # replace the string for with string 4
 print('Python' in course)  # checking to see if the word(string) in exist in course, this will return a boolean value
 
-# Arithmetic operations
+# ----------------------------
+# ARITHMETIC operations
+# ----------------------------
+
 print(10 + 3)  # addition(+), subtract(-), division with floating point number(/)with whole no (//), multiplication(*)
 print(10 % 3)  # remainder of a division
 print(10 ** 3)  # addition exponent, that is raised to the power of
@@ -23,4 +26,25 @@ z = (10 + 3) * 2  # this will give 26 as 10 + 2 will be evaluated first
 print(y)
 print(z)
 
+# ----------------------------------
+# COMPARISON Operators
+# ----------------------------------
 
+x = 3 > 2
+print(x)  # this will give boolean result True or False
+# > - greater than
+# < - less than
+# <= - less than or equals to
+# >= - greater than or equals to
+# != not equal to
+# == equal to
+
+# ----------------------------------
+# LOGICAL Operators - revise the truth table for better knowledge
+# ----------------------------------
+
+price = 25
+print(price > 10 and price < 30) # T and T = True
+print(price > 10 or price < 30)  # T or T = True
+print(price > 30 or price < 25)  # F or F = False
+print(not price > 30)  # True
