a3824a6c07aac5bad46218a72f02535def261c28
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 15:19:42 -0400

    While_Loops

---
+++ While_Loops.py
@@ -0,0 +1,13 @@
+# the code below prints values of i with an increment of 1 and compares it till it meets the condition
+# ----------------------------------
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+# Draw a triangle with asterisk
+# ----------------------------------
+i = 1
+while i <= 10:
+    print(i * '*')
+    i = i + 1
