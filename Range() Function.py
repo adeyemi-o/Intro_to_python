92a7f4f7dac2858332ac25cd8f60a7662bd76af5
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-24 22:47:26 -0400

    more examples

--- Range() Function.py
+++ Range() Function.py
@@ -4,7 +4,38 @@ numbers = range(5, 10, 2)
 for i in numbers:
     print(i)
 
+print()
 # code above simplified
 # ---------------------
 for i in range(5, 10, 2):
     print(i)
+
+# -------------------------
+print()
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+# ------------------------
+print()
+for x in range(1, 5):
+    print(x)
+
+print()
+# factorial
+# --------------------------
+a = 1
+x = int(input("Enter a number: "))
+for i in range(x, 1, -1):
+    a = i * a
+print(a)
+
+
+print()
+# factorial
+# --------------------------
+a = 1
+x = int(input("Enter a number: "))
+for i in range(1, x):
+    a = i * a
+print(a)
