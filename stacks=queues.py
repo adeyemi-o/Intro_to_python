27e2e2e95ff7279f38250d9b6906716dcb38047a
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-10-25 20:15:15 -0400

    stacks & queues

---
+++ stacks=queues.py
@@ -0,0 +1,24 @@
+# STACKS - LIFO - LAST IN FIRST OUT
+# --------------------------------
+# QUEUE - FIFO - FIRST IN FIRST OUT
+
+x = [1, 2, 3]
+print(x)
+
+x.append(1)
+print(x)
+# -------------------
+print()
+stack = ['a', 'b']
+stack.append('c')
+print(stack)
+
+stack.append(['d', 'e'])
+print(stack)
+
+# extend
+# ------------
+stack.extend(['g', 'h'])
+print(stack)
+
+
